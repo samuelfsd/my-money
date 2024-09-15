936efccfb25de798f9845b02d4b89c01152355a7
samuelfsd <samuel.estudo778@gmail.com>
2024-09-15 10:54:05 -0300

    feat: create transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%"> Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">
+                    {/* R$12.000,00 */}
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2024</td>
+            </tr>
+            <tr>
+              <td width="50%"> Feira da semana</td>
+              <td>
+              <PriceHighlight variant="outcome">
+                - R$58,00
+              </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>15/09/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
